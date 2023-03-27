325631b779301b4a97c249015e608ee4967a959f
Saurav Bhandari <120268914+saurav502@users.noreply.github.com>
2023-03-27 13:27:38 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,23 @@
+class API{
+  #secure;
+  url;
+  method;
+  constructor(url){
+      this.url=url;
+      if(url.includes("https")) this.#secure=true;
+      else this.#secure=false;
+      this.method="GET"
+  }
+  isSecure(){
+      return this.#secure;
+  }
+  updateUrl(url){
+      this.url=url
+      if(url.includes("https"))
+      this.#secure=true;
+      else
+      this.#secure=false;
+  }
+}
+
+module.exports = { API }
